30d9cd024803f465405a344ca20412bf20674d87
eric-yates <eric@lumenalabs.com>
2024-03-06 22:32:27 -0700

    patch: export mock task queue

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ export * from './types/nodeTaskQueue.types'
 
 export { default as SpyTaskQueueImpl } from './testDoubles/SpyTaskQueueImpl'
 export * from './testDoubles/SpyTaskQueueImpl'
+
+export { default as MockTaskQueue } from './testDoubles/MockTaskQueue'
+export * from './testDoubles/MockTaskQueue'
