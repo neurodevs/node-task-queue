59b2f58239f612bda3c86f279bfbcf531ea3bf41
eric-yates <eric@lumenalabs.com>
2024-03-06 22:20:43 -0700

    minor: create mock task queue

---
+++ src/testDoubles/MockTaskQueue.ts
@@ -0,0 +1,31 @@
+import { Task, TaskQueue } from '../types/nodeTaskQueue.types'
+
+export default class MockTaskQueue implements TaskQueue {
+	public pushTaskCalls: Task[]
+	public numStartCalls: number
+	public numStopCalls: number
+
+	public constructor() {
+		this.pushTaskCalls = []
+		this.numStartCalls = 0
+		this.numStopCalls = 0
+	}
+
+	public pushTask(task: Task) {
+		this.pushTaskCalls.push(task)
+	}
+
+	public async start() {
+		this.numStartCalls++
+	}
+
+	public async stop() {
+		this.numStopCalls++
+	}
+
+	public resetMock() {
+		this.pushTaskCalls = []
+		this.numStartCalls = 0
+		this.numStopCalls = 0
+	}
+}
