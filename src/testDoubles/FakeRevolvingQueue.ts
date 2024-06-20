008ff8af0bb2889e700105777ac280b2cc84fb70
eric-yates <eric@lumenalabs.com>
2024-06-20 16:21:52 -0600

    patch: add constructor options to fake

--- src/testDoubles/FakeRevolvingQueue.ts
+++ src/testDoubles/FakeRevolvingQueue.ts
@@ -1,7 +1,16 @@
-import { RevolvingQueue, RevolvingTask } from '../types/nodeTaskQueue.types'
+import {
+    RevolvingQueue,
+    RevolvingQueueOptions,
+    RevolvingTask,
+} from '../types/nodeTaskQueue.types'
 
 export default class FakeRevolvingQueue implements RevolvingQueue {
     public pushTaskCalls: RevolvingTask[] = []
+    public constructorOptions?: RevolvingQueueOptions
+
+    public constructor(options?: RevolvingQueueOptions) {
+        this.constructorOptions = options
+    }
 
     public pushTask(task: RevolvingTask) {
         this.pushTaskCalls.push(task)
